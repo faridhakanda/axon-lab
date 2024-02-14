c2227f801ee30f958a55a5038fe6d08b471829fa
farid-akanda <faridakanda1729@gmail.com>
2024-02-14 09:43:05 +0600

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Orbit Lab</h1>
+      <h1>Farid Akanda</h1>
+      <h2>Computer Programmer and Web Developer</h2>
       <p>Orbit lab is one of the newest scientific study center 
         on my local area.
       </p>
